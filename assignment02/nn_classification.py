79571ca13c490a37ba184757eac3b09396ad21ad
Sourmpis <chris.sourmp@gmail.com>
2017-04-21 00:49:35 +0200

    Update nn_classification.py

--- assignment02/nn_classification.py
+++ assignment02/nn_classification.py
@@ -40,6 +40,38 @@ def ex_2_1(input2, target2):
 
 def ex_2_2(input1, target1, input2, target2):
     ## TODO
+    target1 = np.transpose(target1)
+    target1 = target1[0]
+    target2 = np.transpose(target2)
+    target2 = target2[0]
+  
+    
+    acc_train = np.zeros((10,))
+    acc_test = np.zeros((10,))
+    max = -1
+    for i in range(10):
+        nn = MLPClassifier(random_state = i, hidden_layer_sizes=(20, ), activation='tanh', solver='adam', max_iter=1000)
+        
+        model = nn.fit(input1,target1)
+        acc_train[i] = model.score(input1,target1)
+        acc_test[i] = model.score(input2,target2)
+        if acc_test[i]>max:
+            max = acc_test[i]
+            y_predict = model.predict(input2)
+            C = confusion_matrix(target2,y_predict)
+    k=0
+    for i,a in enumerate(target2):
+        if a != y_predict[i] and k <20:
+            plot_image(input2[i])
+            k = k+1
+            
+            
+    hidden_layer_weights = model.coefs_ 
+    plot_hidden_layer_weights(hidden_layer_weights[0])   
+    
+    plot_histogram_of_acc(acc_train, acc_test)
+    print(C)
+        
     pass
 
 
